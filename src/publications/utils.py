8705b8060605ff735fc26d668605f4d961fee0f9
Azim Maksatbek uulu <bzrbvazm@gmail.com>
2026-04-16 21:20:00 +0200

    Cleaning the commit from api

--- src/publications/utils.py
+++
@@ -1,9 +0,0 @@
-from Bio import Entrez
-import pandas as pd
-from tqdm import tqdm
-import numpy as np
-import matplotlib.pyplot as plt
-
-def yearly_distribution_hist(df):
-    df['year'].hist()
-    plt.show()
